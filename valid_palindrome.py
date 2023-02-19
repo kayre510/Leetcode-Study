fc232d8568d66f98e47e6cd0ec6f04fcf431a73d
kayre <kayresantos@gmail.com>
2023-02-18 16:22:55 -0800

    lc

---
+++ valid_palindrome.py
@@ -0,0 +1,7 @@
+    def isPalindrome(self, s):
+        new_string = ''
+        for character in s:
+            character.isalnum()
+            new_string += character
+        new_string = new_string[::-1]
+
